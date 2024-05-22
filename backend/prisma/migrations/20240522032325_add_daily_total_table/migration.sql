35f14519359fb01a19a3c9d894651118524074ca
Drewthurm21 <drewthurm21@gmail.com>
2024-05-21 22:40:01 -0500

    added DailyTotal table and created migration.  moving on to testing

---
+++ backend/prisma/migrations/20240522032325_add_daily_total_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "DailyTotal" (
+    "id" SERIAL NOT NULL,
+    "account_id" INTEGER NOT NULL,
+    "date" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "total_deposit" DECIMAL(18,2) NOT NULL DEFAULT 0.00,
+    "total_witdrawal" DECIMAL(18,2) NOT NULL DEFAULT 0.00,
+    "total_transfer" DECIMAL(18,2) NOT NULL DEFAULT 0.00,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DailyTotal_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DailyTotal_account_id_date_key" ON "DailyTotal"("account_id", "date");
+
+-- AddForeignKey
+ALTER TABLE "DailyTotal" ADD CONSTRAINT "DailyTotal_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "Account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
