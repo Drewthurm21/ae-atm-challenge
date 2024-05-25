988c4a393f486b5ad9934a6c90a36d279babd790
Drewthurm21 <drewthurm21@gmail.com>
2024-05-25 13:57:31 -0500

    fixed test data to sim real transaction values and fixed logic in relationships test. Updated port selection in app.js to use alternate for testing. removed db drop from pretest setup in favor of global teardown.

--- backend/jest.teardown.js
+++ backend/jest.teardown.js
@@ -1,23 +1,8 @@
 const { execSync } = require('child_process');
-const { PrismaClient } = require('@prisma/client');
-const { server } = require('./src/app')
-const dotenv = require('dotenv');
-
-dotenv.config({ path: '.env.test' });
-
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.TEST_DATABASE_URL,
-    },
-  },
-});
 
 module.exports = async () => {
   try {
-    await prisma.$disconnect();
-    server.close();
-
+    console.log('Tearing down test db');
     // Connect to the 'postgres' database to drop the 'testdb' database
     execSync(`docker exec challenge_db psql -U user -d postgres -c "DROP DATABASE IF EXISTS testdb;"`, { stdio: 'inherit' });
   } catch (error) {
