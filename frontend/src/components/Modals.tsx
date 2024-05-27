ee1e560ab81c5d0e16672eb6d06bb50b34d0ca70
Drewthurm21 <drewthurm21@gmail.com>
2024-05-27 01:16:16 -0500

    replaced blur effect in modal background with more impactful background image

--- frontend/src/components/Modals.tsx
+++ frontend/src/components/Modals.tsx
@@ -48,7 +48,7 @@ const SpringModal = ({
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         onClick={closeModal}
-        className="bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer"
+        className="bg-modal-overlay bg-repeat p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer"
       >
         <motion.div
           initial={{ scale: 0, rotate: "12.5deg" }}
